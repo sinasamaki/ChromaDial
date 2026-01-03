517b1445c5689ab168078a93cf7256ef69acc279
sinasamaki <sinasamaki@protonmail.com>
2026-01-03 12:48:02 +0100

    add compose hot reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
 //    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.composeHotReload) apply false
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 //    alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
 }
 
 group = "com.sinasamaki.chroma.dial"
