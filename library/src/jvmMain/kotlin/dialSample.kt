003666c35fdbf025ba4fea845e850d6d11307c47
sinasamaki <sinasamaki@protonmail.com>
2026-01-09 13:18:14 +0100

    timer dial

--- library/src/jvmMain/kotlin/dialSample.kt
+++ library/src/jvmMain/kotlin/dialSample.kt
@@ -1,8 +1,12 @@
 package com.sinasamaki.chroma.dial
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -23,6 +27,7 @@ import androidx.compose.material.icons.filled.Api
 import androidx.compose.material.icons.filled.BrowseGallery
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.ModeNight
+import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -36,8 +41,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -50,6 +57,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.singleWindowApplication
 import androidx.compose.ui.zIndex
+import kotlin.math.absoluteValue
 
 fun main() = singleWindowApplication {
 
@@ -351,7 +359,10 @@ fun DialExample4() {
                                     drawLine(
                                         color = White,
                                         start = position,
-                                        end = position + Offset(0f, if (degrees in (-91f)..(-89f)) 30f else 10f)
+                                        end = position + Offset(
+                                            0f,
+                                            if (degrees in (-91f)..(-89f)) 30f else 10f
+                                        )
                                     )
                                 }
                             }
@@ -420,72 +431,84 @@ fun DialExample4() {
                                 color = Green500,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             1 -> Text(
                                 text = "P",
                                 fontSize = 10.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             2 -> Text(
                                 text = "A",
                                 fontSize = 10.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             3 -> Text(
                                 text = "S",
                                 fontSize = 10.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             4 -> Text(
                                 text = "M",
                                 fontSize = 10.sp,
                                 color = Black,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             5 -> Icon(
                                 imageVector = Icons.Default.Api,
                                 contentDescription = null,
                                 tint = White,
                                 modifier = Modifier.size(12.dp)
                             )
+
                             6 -> Icon(
                                 imageVector = Icons.Default.BrowseGallery,
                                 contentDescription = null,
                                 tint = White,
                                 modifier = Modifier.size(12.dp)
                             )
+
                             7 -> Text(
                                 text = "B",
                                 fontSize = 10.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             8 -> Text(
                                 text = "C",
                                 fontSize = 10.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             9 -> Icon(
                                 imageVector = Icons.Default.ModeNight,
                                 contentDescription = null,
                                 tint = White,
                                 modifier = Modifier.size(12.dp)
                             )
+
                             10 -> Text(
                                 text = "Video",
                                 fontSize = 8.sp,
                                 color = White,
                                 fontFamily = FontFamily.Monospace,
                             )
+
                             11 -> Icon(
                                 imageVector = Icons.Default.Face,
                                 contentDescription = null,
                                 tint = White,
                                 modifier = Modifier.size(12.dp)
                             )
+
                             else -> Text(
                                 text = "$index",
                                 fontSize = 10.sp,
@@ -557,26 +580,47 @@ fun DialExample5() {
 @Composable
 fun DialExample6() {
     Column(
+        modifier = Modifier
+            .background(
+                color = Zinc50,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Text(
-            "Thick Track - Green",
-            color = White,
+            "60 min timer",
+            color = Black,
             fontSize = 14.sp,
             fontWeight = FontWeight.Medium
         )
-        var degree by remember { mutableFloatStateOf(270f) }
+        var degree by remember { mutableFloatStateOf(0f) }
+        val animatedDegree by animateFloatAsState(
+            targetValue = degree,
+            animationSpec = spring(
+                stiffness = Spring.StiffnessHigh,
+            )
+        )
+
+        Text(
+            text = "${(degree.absoluteValue / 6).toInt()} min",
+            modifier = Modifier.padding(12.dp),
+            fontSize = 28.sp,
+            color = Zinc800,
+            fontFamily = FontFamily.Monospace
+        )
+
         Dial(
-            degree = degree,
+            degree = animatedDegree,
             onDegreeChanged = { degree = it },
-            modifier = Modifier.size(200.dp),
-            startDegrees = 0f,
-            sweepDegrees = 360f,
+            modifier = Modifier.size(300.dp),
+            startDegrees = -360f * 1,
+            sweepDegrees = 360f * 1,
+            steps = 59 * 1,
             thumb = {
                 Box(
-                    Modifier.size(24.dp)
-                        .background(color = Green500, shape = CircleShape)
+                    Modifier.fillMaxSize()
                 )
             },
             track = {
@@ -584,13 +628,135 @@ fun DialExample6() {
                     Modifier
                         .fillMaxSize()
                         .drawBehind {
+                            val strokeWidth = 120.dp
                             drawCircle(
-                                color = Green500.copy(alpha = .25f),
-                                style = Stroke(width = 70.dp.toPx()),
-                                radius = (size.width / 2) - 35.dp.toPx()
+                                color = Zinc400.copy(alpha = .01f),
+                                style = Stroke(width = strokeWidth.toPx()),
+                                radius = (size.width / 2) - (strokeWidth / 2).toPx()
+                            )
+                            val rect = Rect(
+                                center = center,
+                                radius = (size.width / 2) - (strokeWidth / 2).toPx(),
+                            )
+                            drawArc(
+                                brush = Brush.radialGradient(
+                                    colors = listOf(
+                                        Red600,
+                                        Red500,
+                                    )
+                                ),
+                                startAngle = -90f,
+                                sweepAngle = it.degree,
+                                topLeft = rect.topLeft,
+                                size = rect.size,
+                                useCenter = false,
+                                style = Stroke(
+                                    width = strokeWidth.toPx(),
+                                )
+
+                            )
+                            rotate(
+                                degrees = it.degree
+                            ) {
+                                drawEveryStep(
+                                    degreeRange = 0f..360f,
+                                    radius = size.width * .5f,
+                                    padding = 25.dp,
+                                    steps = 11,
+                                ) { position, degree, _ ->
+                                    rotate(
+                                        degrees = degree,
+                                        pivot = position
+                                    ) {
+                                        drawLine(
+                                            color = Zinc800,
+                                            start = position,
+                                            end = position + Offset(0f, 20f),
+                                            strokeWidth = 5f,
+                                            cap = StrokeCap.Round,
+                                        )
+                                    }
+                                }
+                                drawEveryStep(
+                                    degreeRange = 0f..360f,
+                                    radius = size.width * .5f,
+                                    padding = 25.dp,
+                                    steps = 59,
+                                ) { position, degree, _ ->
+                                    rotate(
+                                        degrees = degree,
+                                        pivot = position
+                                    ) {
+                                        drawLine(
+                                            color = Zinc800,
+                                            start = position,
+                                            end = position + Offset(0f, 10f),
+                                            strokeWidth = 3f,
+                                        )
+                                    }
+                                }
+
+                            }
+
+                            if (degree == 0f) {
+                                drawCircle(
+                                    color = Red500,
+                                    radius = 8.dp.toPx()
+                                )
+                                drawLine(
+                                    color = Red500,
+                                    start = Offset(center.x, 45.dp.toPx()),
+                                    end = center,
+                                    strokeWidth = 4.dp.toPx(),
+                                    cap = StrokeCap.Round,
+                                )
+                            }
+                        }
+                ) {
+                    Column(
+                        modifier = Modifier.align(Alignment.Center)
+                    ) {
+                        AnimatedVisibility(
+                            visible = degree != 0f,
+                            enter = scaleIn(),
+                            exit = scaleOut(),
+                        ) {
+                            Image(
+                                imageVector = Icons.Rounded.PlayArrow,
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(White),
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .size(48.dp)
+                                    .background(
+                                        color = Zinc900,
+                                        shape = CircleShape,
+                                    )
                             )
                         }
-                )
+                    }
+
+                }
+                StepContent(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            rotationZ = it.degree
+                        }
+                        .fillMaxSize(),
+                    degreeRange = 0f..360f,
+                    steps = 11,
+                ) { index, position, degree, _ ->
+                    if (index < 12) {
+                        Text(
+                            text = "${index * 5}",
+                            modifier = Modifier
+                                .rotate(-90f)
+                                .padding(horizontal = 4.dp),
+                            color = Zinc950,
+                            fontFamily = FontFamily.Monospace,
+                        )
+                    }
+                }
             }
         )
     }
