17f86d308c947fc04fceca165e61ff3745b647c6
sinasamaki <sinasamaki@protonmail.com>
2026-01-19 22:36:42 +0100

    add workflow for publishing docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://sinasamaki.github.io',
+    base: '/chromadial',
 	integrations: [
 		starlight({
 			title: 'ChromaDial',
