edd1c3252fb79fef869378625cd7171c567325ef
sinasamaki <sinasamaki@protonmail.com>
2026-01-26 09:04:18 +0100

    clean up gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
-
-//    alias(libs.plugins.androidApplication) apply false
-//    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.composeHotReload) apply false
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.vanniktech.mavenPublish)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
 }
 
 group = "com.sinasamaki"
